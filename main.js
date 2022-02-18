6647c7048a0c608fea1b8761c0a204d61ef97c6f
larsencs <quietus77@gmail.com>
2022-02-18 10:44:12 -0600

    Added an array and a foor loop simply to make things more complicated

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("This is a complicated program")
+
+const improvements = [
+"This"
+, "is"
+, "a"
+, "very"
+, "complicated"
+, "program"
+, "now"]
+
+let complications = ""
+
+for(let i = 0; i<improvements.length; i++){
+    complications += improvements[i] + " "
+}
+
+console.log(complications)
